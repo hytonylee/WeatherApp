202dc094b2f214710e16e0deefde7714cc41b130
Tony Lee <hy.tony.lee@gmail.com>
2019-09-12 00:13:58 -0700

    Re-track axio

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Header from './components/Header';
 import InputForm from './components/InputForm';
 import WeatherGroup from './components/WeatherGroup';
-import axios from 'axios';
+
+const API = 'https://api.openweathermap.org/data/2.5/weather?q=';
+let city = 'Vancouver'
+let country = ''
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            city: '',
-            country: '',
             weather: [],
-            isLoading: false,
-            imageURL: ''
+            isLoading: false
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -22,8 +22,9 @@ class App extends React.Component {
         event.preventDefault();
         city = this.state.city;
         country = this.state.country;
-        alert("You are submitting " + this.state.city + ', ' + this.state.country);
+        // alert("You are submitting " + this.state.city + ', ' + this.state.country);
     }
+
 
     handleChange(event) {
         let name = event.target.name;
@@ -34,26 +35,12 @@ class App extends React.Component {
         })
     }
 
-    // componentDidMount() {
-    //     axios.get('https://dog.ceo/api/breeds/image/random')
-    //         .then(response => {
-    //             this.setState({ imageURL: response.data.message })
-    //         })
-    //         .catch(error => {
-    //             console.log(error);
-    //         });
-    // }
-
     render() {
-        // const { imageURL } = this.state;
         return (
             <div>
                 <Header />
-                {/* <img src={imageURL} /> */}
                 <InputForm
-                    onSubmit={this.handleSubmit}
-                    onChange={this.handleChange}
-                />
+                    onSubmit={this.handleSubmit} onChange={this.handleChange} />
                 <WeatherGroup />
             </div>
         )
