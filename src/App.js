002a79c251077bcbeb53d1312edb5bf19889d7a0
Tony Lee <hy.tony.lee@gmail.com>
2019-09-12 00:03:34 -0700

    Add axios

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import Header from './components/Header';
 import InputForm from './components/InputForm';
 import WeatherGroup from './components/WeatherGroup';
+import axios from 'axios';
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             city: '',
-            coutnry: '',
-            weather: ''
+            country: '',
+            weather: [],
+            isLoading: false,
+            imageURL: ''
         }
-
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleSubmit(event) {
         event.preventDefault();
+        city = this.state.city;
+        country = this.state.country;
         alert("You are submitting " + this.state.city + ', ' + this.state.country);
     }
 
@@ -30,11 +34,26 @@ class App extends React.Component {
         })
     }
 
+    // componentDidMount() {
+    //     axios.get('https://dog.ceo/api/breeds/image/random')
+    //         .then(response => {
+    //             this.setState({ imageURL: response.data.message })
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+    // }
+
     render() {
+        // const { imageURL } = this.state;
         return (
             <div>
                 <Header />
-                <InputForm onSubmit={this.handleSubmit} onChange={this.handleChange} />
+                {/* <img src={imageURL} /> */}
+                <InputForm
+                    onSubmit={this.handleSubmit}
+                    onChange={this.handleChange}
+                />
                 <WeatherGroup />
             </div>
         )
